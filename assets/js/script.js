58f3ad69d264ef116023ffd19e9a2df9ed12c377
RoughGhost <stephendlouis6@gmail.com>
2022-06-14 14:54:22 +0100

    reviewing

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// /* ========================   TYPING ANIMATION ==============   */
+// import Typed from "typed.js";
+// var options = new Typed({
+//   strings: ["Web Developer, Copywriter, Maketing Specialist, Graphic Designer"],
+//   typeSpeed: 100,
+//   BackSpeed: 60,
+//   loop: true,
+// });
+
+// var typed = new Typed(".typing", options);
+
+// import Typewriter from "typewriter-effect/dist/core";
+
+// new Typewriter(".typing", {
+//   strings: ["Web Developer", "Graphic Designer"],
+//   autoStart: true,
+// });
